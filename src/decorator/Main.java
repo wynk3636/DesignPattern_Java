4c226081e88704e90c69696e12ce635dfa14e01f
ishida <hand3636hand@gmail.com>
2021-04-23 09:08:55 +0900

    decorator

---
+++ src/decorator/Main.java
@@ -0,0 +1,25 @@
+package decorator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Display d1 = new StringDisplay("hello");
+		Display d2 = new SideBorder(d1, '#');
+		Display d3 = new FullBorder(d2);
+		d1.show();
+		d2.show();
+		d3.show();
+		
+		Display d4 = new SideBorder(new FullBorder(
+					new FullBorder(
+							new SideBorder(
+									new FullBorder(
+											new StringDisplay("こん"))
+									, '*')
+							)
+					)
+				, '/');
+		d4.show();
+	}
+
+}
---
+++ src/decorator/SideBorder.java
@@ -0,0 +1,26 @@
+package decorator;
+
+public class SideBorder extends Border {
+	
+	private char borderChar;
+	public SideBorder(Display disp, char ch) {
+		super(disp);
+		this.borderChar=ch;
+	}
+
+	@Override
+	public int getColumns() {
+		return 1+display.getColumns()+1;
+	}
+
+	@Override
+	public int getRows() {
+		return display.getRows();
+	}
+
+	@Override
+	public String getRowText(int row) {
+		return borderChar + display.getRowText(row) + borderChar;
+	}
+
+}
